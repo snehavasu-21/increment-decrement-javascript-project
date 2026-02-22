406b3447f27e5a0500dcbb642cc9302e625c72fb
snehavasu-21 <jha.snehavasu@gmail.com>
2026-02-22 22:05:48 +0530

    Initial commit

---
+++ inc_dec.js/script.js
@@ -0,0 +1,33 @@
+let count = 0;
+
+function update(){
+    document.getElementById("adding").innerText = count;
+}
+
+function increment(){
+    count++;
+    update();
+}
+
+function decrement(){
+    count--;
+    update();
+}
+
+function oddIncrement(){
+    if(count % 2 !== 0){
+        count++;
+        update();
+    }else{
+        alert("Number is not Odd");
+    }
+}
+
+function evenIncrement(){
+    if(count % 2 === 0){
+        count++;
+        update();
+    }else{
+        alert("Number is not Even");
+    }
+}
